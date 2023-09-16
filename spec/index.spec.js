e5b17968c129bf7cc5b5867566d1d67ed6be12f3
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2023-09-16 16:28:09 +0900

    test: update jasmine

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -4,8 +4,8 @@
  * Module dependencies.
  */
 
-var cordova = require('./helper/cordova'),
-    BarcodeScanner = require('../www/barcodescanner'),
+var cordova = require("./helper/cordova"),
+    BarcodeScanner = require("../www/barcodescanner"),
     execSpy,
     execWin,
     options;
@@ -14,60 +14,70 @@ var cordova = require('./helper/cordova'),
  * Specification.
  */
 
-describe('phonegap-plugin-barcodescanner', function () {
+describe("phonegap-plugin-barcodescanner", function () {
     beforeEach(function () {
         execWin = jasmine.createSpy();
-        execSpy = spyOn(cordova.required, 'cordova/exec').andCallFake(execWin);
+        execSpy = spyOn(cordova.required, "cordova/exec").and.callFake(execWin);
     });
 
-    describe('BarcodeScanner', function () {
-      it("BarcodeScanner plugin should exist", function() {
-          expect(BarcodeScanner).toBeDefined();
-          expect(typeof BarcodeScanner == 'object').toBe(true);
-      });
+    describe("BarcodeScanner", function () {
+        it("BarcodeScanner plugin should exist", function () {
+            expect(BarcodeScanner).toBeDefined();
+            expect(typeof BarcodeScanner == "object").toBe(true);
+        });
 
-      it("should contain a scan function", function() {
-          expect(BarcodeScanner.scan).toBeDefined();
-          expect(typeof BarcodeScanner.scan == 'function').toBe(true);
-      });
+        it("should contain a scan function", function () {
+            expect(BarcodeScanner.scan).toBeDefined();
+            expect(typeof BarcodeScanner.scan == "function").toBe(true);
+        });
 
-      it("should contain an encode function", function() {
-          expect(BarcodeScanner.encode).toBeDefined();
-          expect(typeof BarcodeScanner.encode == 'function').toBe(true);
-      });
+        it("should contain an encode function", function () {
+            expect(BarcodeScanner.encode).toBeDefined();
+            expect(typeof BarcodeScanner.encode == "function").toBe(true);
+        });
 
-      it("should contain three DestinationType constants", function() {
-          expect(BarcodeScanner.Encode.TEXT_TYPE).toBe("TEXT_TYPE");
-          expect(BarcodeScanner.Encode.EMAIL_TYPE).toBe("EMAIL_TYPE");
-          expect(BarcodeScanner.Encode.PHONE_TYPE).toBe("PHONE_TYPE");
-          expect(BarcodeScanner.Encode.SMS_TYPE).toBe("SMS_TYPE");
-      });
+        it("should contain three DestinationType constants", function () {
+            expect(BarcodeScanner.Encode.TEXT_TYPE).toBe("TEXT_TYPE");
+            expect(BarcodeScanner.Encode.EMAIL_TYPE).toBe("EMAIL_TYPE");
+            expect(BarcodeScanner.Encode.PHONE_TYPE).toBe("PHONE_TYPE");
+            expect(BarcodeScanner.Encode.SMS_TYPE).toBe("SMS_TYPE");
+        });
     });
 
-    describe('BarcodeScanner instance', function () {
-        describe('cordova.exec', function () {
-            it('should call cordova.exec on next process tick', function (done) {
-                BarcodeScanner.scan(function() {}, function() {}, {});
+    describe("BarcodeScanner instance", function () {
+        describe("cordova.exec", function () {
+            it("should call cordova.exec on next process tick", function (done) {
+                BarcodeScanner.scan(
+                    function () {},
+                    function () {},
+                    {}
+                );
                 setTimeout(function () {
                     expect(execSpy).toHaveBeenCalledWith(
                         jasmine.any(Function),
                         jasmine.any(Function),
-                        'BarcodeScanner',
-                        'scan',
+                        "BarcodeScanner",
+                        "scan",
                         jasmine.any(Object)
                     );
                     done();
                 }, 100);
             });
 
-            it('should call cordova.exec on next process tick', function (done) {
-                BarcodeScanner.encode("", "",function() {}, function() {}, {});
+            it("should call cordova.exec on next process tick", function (done) {
+                BarcodeScanner.encode(
+                    "",
+                    "",
+                    function () {},
+                    function () {},
+                    {}
+                );
                 setTimeout(function () {
                     expect(execSpy).toHaveBeenCalledWith(
                         jasmine.any(Function),
                         jasmine.any(Function),
-                        'BarcodeScanner',
-                        'encode',
+                        "BarcodeScanner",
+                        "encode",
                         jasmine.any(Object)
                     );
                     done();
